74d66283fb97972b6eb11db6b5e18ce0cd02f35f
Moreen Hettihewa <moreenmadhushani>
2023-11-17 22:53:38 +0530

    Updated Portforlio info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,19 @@ import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  LINKEDIN_URL = 'https://www.linkedin.com/in/moreenhettihewa/';
+  GITHUB_URL = 'https://github.com/moreenhettihewa';
 
-  constructor(private _formBuilder:FormBuilder){}
+  constructor(private _formBuilder: FormBuilder) {}
 
+  openLinkedIn(): void {
+    window.open(this.LINKEDIN_URL);
+  }
 
+  openGitHub(): void {
+    window.open(this.GITHUB_URL);
+  }
 }
